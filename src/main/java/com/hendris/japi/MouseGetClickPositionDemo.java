4736826d39878fbba2a89ea298c131cfdbd0a37f
Hendris Koya <hendriskoya@gmail.com>
2016-06-09 18:42:36 -0300

    novas alteracoes

--- src/main/java/com/hendris/japi/MouseGetClickPositionDemo.java
+++ src/main/java/com/hendris/japi/MouseGetClickPositionDemo.java
@@ -1,7 +1,11 @@
 package com.hendris.japi;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.*;
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.mouse.NativeMouseEvent;
@@ -16,6 +20,96 @@ public class MouseGetClickPositionDemo implements NativeMouseListener {
 	 * @throws InterruptedException
      */
 	public static void main(String[] args) throws InterruptedException {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new MouseGetClickPositionDemo();
+			}
+		});
+	}
+
+	public void nativeMouseClicked(NativeMouseEvent arg0) {
+
+	}
+
+	public void nativeMousePressed(NativeMouseEvent e) {
+		System.out.println(e.getX());
+		System.out.println(e.getY());
+		if (selectedField != null) {
+			selectedField.setText("" + e.getX() + ", " + e.getY());
+			selectedField = null;
+		}
+	}
+
+	public void nativeMouseReleased(NativeMouseEvent arg0) {
+	}
+
+	public MouseGetClickPositionDemo() {
+		try {
+			start();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(200, 200);
+
+		addComponents(frame);
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	private JTextField selectedField;
+
+	public void addComponents(JFrame frame) {
+		Container container = frame.getContentPane();
+		GridLayout gridLayout = new GridLayout(0, 2);
+		container.setLayout(gridLayout);
+
+		JTextField field1 = new JTextField();
+		JTextField field2 = new JTextField();
+		JTextField field3 = new JTextField();
+
+		JButton botao1 = new JButton("Botão 1");
+		botao1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				selectedField = field1;
+			}
+		});
+
+		JButton botao2 = new JButton("Botão 2");
+		botao2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				selectedField = field2;
+			}
+		});
+
+		JButton botao3 = new JButton("Botão 3");
+		botao3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				selectedField = field3;
+			}
+		});
+
+
+		container.add(botao1);
+		container.add(field1);
+
+
+		container.add(botao2);
+		container.add(field2);
+
+		container.add(botao3);
+		container.add(field3);
+
+	}
+
+	public void start() throws InterruptedException {
 		Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
 		logger.setLevel(Level.WARNING);
 		try {
@@ -25,7 +119,8 @@ public class MouseGetClickPositionDemo implements NativeMouseListener {
 
 			System.exit(1);
 		}
-		GlobalScreen.addNativeMouseListener(new MouseGetClickPositionDemo());
+		//GlobalScreen.addNativeMouseListener(new MouseGetClickPositionDemo());
+		GlobalScreen.addNativeMouseListener(this);
 
 		Thread.sleep(1000);
 		NativeMouseEvent me = new  NativeMouseEvent(
@@ -47,17 +142,5 @@ public class MouseGetClickPositionDemo implements NativeMouseListener {
 
 		GlobalScreen.postNativeEvent(me);
 		*/
-	}
-
-	public void nativeMouseClicked(NativeMouseEvent arg0) {
-
-	}
-
-	public void nativeMousePressed(NativeMouseEvent e) {
-		System.out.println(e.getX());
-		System.out.println(e.getY());
-	}
-
-	public void nativeMouseReleased(NativeMouseEvent arg0) {
 	}
 }
--- src/main/java/com/hendris/winapi/WinAPI.java
+++ src/main/java/com/hendris/winapi/WinAPI.java
@@ -2,6 +2,7 @@ package com.hendris.winapi;
 
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.platform.win32.WinUser;
 
@@ -57,9 +58,7 @@ public class WinAPI {
          int count = 0;
 
          public boolean callback(HWND hWnd, Pointer lParam) {
-            byte[] windowText = new byte[512];
-            user32.GetWindowTextA(hWnd, windowText, 512);
-            String wText = Native.toString(windowText);
+            String wText = getCaption(hWnd);
 
             // get rid of this if block if you want all windows regardless of whether
             // or not they have text
@@ -81,7 +80,7 @@ public class WinAPI {
                   System.out.println(String.format("%d %d %d %d", rect[0], rect[1], rect[2], rect[3]));
                }
 
-               windows.add(new Window(wText, hWnd, rect));
+               windows.add(new Window(wText, hWnd, rect, null));
             }
             return true;
          }
@@ -91,47 +90,20 @@ public class WinAPI {
 
 
 
-   public void teste() {
+   public void teste(String windowCaption) {
       System.out.println("init main");
       final User32 user32 = User32.INSTANCE;
 
-      final WinUser.WNDENUMPROC wndenumproc = new WinUser.WNDENUMPROC() {
-         public boolean callback(HWND hwnd, Pointer pointer) {
-            byte[] windowText = new byte[512];
-            user32.GetClassNameA(hwnd, windowText, 512);
-            String className = Native.toString(windowText);
-//            System.out.println("child: " + Native.toString(windowText));
-            if (className.equals("PokerStarsButtonClass")) {
-               System.out.println("child: " + Native.toString(windowText));
 
-               int id = user32.GetDlgCtrlID(hwnd);
-               System.out.println("id: " + id);
 
-               byte[] text = new byte[512];
-               int n = user32.GetDlgItemText(hwnd, 1000, text, 512);
-               System.out.println("n: " + n + ", " + Native.toString(text));
-            }
-            /*user32.GetWindowTextA(hwnd, windowText, 512);
-            String wText = Native.toString(windowText);
-
-            if (wText.isEmpty()) {
-               return true;
-            }
-
-            System.out.println("childWindow: " + hwnd + ", Text: " + wText);*/
-
-            return true;
-         }
-      };
-
+      HWND mainHWnd;
 
       user32.EnumWindows(new WinUser.WNDENUMPROC() {
          int count = 0;
 
          public boolean callback(HWND hWnd, Pointer arg1) {
-            byte[] windowText = new byte[512];
-            user32.GetWindowTextA(hWnd, windowText, 512);
-            String wText = Native.toString(windowText);
+
+            String wText = getCaption(hWnd);
 
             // get rid of this if block if you want all windows regardless of whether
             // or not they have text
@@ -141,7 +113,7 @@ public class WinAPI {
             boolean isWindow = user32.IsWindow(hWnd);
             boolean isWindowVisible = user32.IsWindowVisible(hWnd);
 //            if (isWindowVisible && wText.contains(username)) {
-            if (isWindowVisible && wText.toUpperCase().contains("NOTEPAD")) {
+            if (isWindowVisible && wText.toUpperCase().contains(windowCaption.toUpperCase())) {
                System.out.println("Found window with text " + hWnd + ", total " + ++count
                        + " Text: " + wText + " IsWindow: " + isWindow);
 
@@ -156,12 +128,67 @@ public class WinAPI {
                   System.out.println(String.format("%d %d %d %d", rect[0], rect[1], rect[2], rect[3]));
                }
 
-               /*System.out.println("");
-               user32.EnumChildWindows(hWnd, wndenumproc, null);
-               System.out.println("");*/
+               enumChildWindows(hWnd, user32);
             }
             return true;
          }
       }, null);
+   }
+
+   private void enumChildWindows(final HWND mainHWnd, User32 user32) {
+
+      final HWND[] teste = new HWND[1];
+
+      final WinUser.WNDENUMPROC wndenumproc = new WinUser.WNDENUMPROC() {
+         public boolean callback(HWND hwnd, Pointer pointer) {
+            byte[] windowText = new byte[512];
+            user32.GetClassNameA(hwnd, windowText, 512);
+            String className = Native.toString(windowText);
+//            System.out.println("child: " + Native.toString(windowText));
+
+            if (className.equals("CalcFrame")) {
+               System.out.println("CalcFrame set");
+               teste[0] = hwnd;
+            }
+
+            //if (className.equals("PokerStarsButtonClass")) {
+            System.out.println("child: " + Native.toString(windowText));
+
+            String wText = getCaption(hwnd);
+            System.out.println("wText: " + wText);
+
+            int id = user32.GetDlgCtrlID(hwnd);
+            System.out.println("id: " + id);
+
+            HWND hwndItem = user32.GetDlgItem(mainHWnd, id);
+            System.out.println("handle: " + hwndItem);
+
+            byte[] text = new byte[512];
+            int n = user32.GetDlgItemTextA(teste[0], id, text, 512);
+            System.out.println("n: " + n + ", " + Native.toString(text));
+            //}
+
+            /*user32.GetWindowTextA(hwnd, windowText, 512);
+            String wText = Native.toString(windowText);
+
+            if (wText.isEmpty()) {
+               return true;
+            }
+
+            System.out.println("childWindow: " + hwnd + ", Text: " + wText);*/
+
+            return true;
+         }
+      };
+
+      System.out.println("\nstart EnumChildWindows");
+      user32.EnumChildWindows(mainHWnd, wndenumproc, null);
+      System.out.println("");
+   }
+
+   private String getCaption(HWND hWnd) {
+      byte[] windowText = new byte[512];
+      user32.GetWindowTextA(hWnd, windowText, 512);
+      return Native.toString(windowText);
    }
 }
